8d95a564c2297d73167dde4c7bc9c095fc80046d
Neyderyesid <neyderpereaurrutia92@gmail.com>
2026-05-11 17:25:43 -0500

    Fix Mongo Atlas URI

--- includes/db.php
+++ includes/db.php
@@ -1,20 +1,16 @@
 <?php
-// ── Iniciar sesión ──
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// ── Cargar Composer ──
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
 
-    // ── Conexión MongoDB Atlas ──
     $client = new MongoDB\Client(
-        "mongodb+srv://neyderpereaurrutia92_db_user:erPFIEblB7MEglyQ@cluster0.fuvknik.mongodb.net/reposteria_db?retryWrites=true&w=majority&tls=true"
+        "mongodb+srv://neyderpereaurrutia92_db_user:erPFIEblB7MEglyQ@cluster0.fuvknik.mongodb.net/reposteria_db?retryWrites=true&w=majority&appName=Cluster0"
     );
 
-    // ── Base de datos ──
     $db = $client->reposteria_db;
 
 } catch (Exception $e) {
